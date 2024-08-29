792eb5cbeb440ab7d603524d488a6e51c5221e01
Yoshinori Takada <is0263rf@gmail.com>
2024-08-29 14:45:06 +0900

    rename lxw_chart_series to lxwChartSeries.

--- Sources/xlsxwriter/Series.swift
+++ Sources/xlsxwriter/Series.swift
@@ -10,27 +10,27 @@ import libxlsxwriter
 /// Struct to represent an Excel chart data series.
 public struct Series {
 
-  let lxw_chart_series: UnsafeMutablePointer<lxw_chart_series>
+  let lxwChartSeries: UnsafeMutablePointer<lxw_chart_series>
   init(_ lxw_chart_series: UnsafeMutablePointer<lxw_chart_series>) {
-    self.lxw_chart_series = lxw_chart_series
+    self.lxwChartSeries = lxw_chart_series
   }
   /// Set the name of a chart series range.
   @discardableResult public func set(name: String) -> Series {
-    let _ = name.withCString { chart_series_set_name(lxw_chart_series, $0) }
+    let _ = name.withCString { chart_series_set_name(lxwChartSeries, $0) }
     return self
   }
 
   /// The function is used to specify the the series marker:
   @discardableResult public func set(marker: Int, size: Int) -> Series {
-    chart_series_set_marker_type(lxw_chart_series, UInt8(marker))
-    chart_series_set_marker_size(lxw_chart_series, UInt8(size))
+    chart_series_set_marker_type(lxwChartSeries, UInt8(marker))
+    chart_series_set_marker_size(lxwChartSeries, UInt8(size))
     return self
   }
 
   /// Set a series "values" range using row and column values.
   @discardableResult public func values(sheet: Worksheet, range: Range) -> Series {
     let _ = sheet.name.withCString {
-      chart_series_set_values(lxw_chart_series, $0, range.row, range.col, range.row2, range.col2)
+      chart_series_set_values(lxwChartSeries, $0, range.row, range.col, range.row2, range.col2)
     }
     return self
   }
@@ -38,31 +38,31 @@ public struct Series {
   @discardableResult public func categories(sheet: Worksheet, range: Range) -> Series {
     let _ = sheet.name.withCString {
       chart_series_set_categories(
-        lxw_chart_series, $0, range.row, range.col, range.row2, range.col2)
+        lxwChartSeries, $0, range.row, range.col, range.row2, range.col2)
     }
     return self
   }
   /// Smooth a line or scatter chart series.
   @discardableResult public func set(smooth: Bool) -> Series {
     let smooth: UInt8 = smooth ? 1 : 0
-    chart_series_set_smooth(lxw_chart_series, smooth)
+    chart_series_set_smooth(lxwChartSeries, smooth)
     return self
   }
   /// Set a series name formula using row and column values.
   @discardableResult public func name(sheet: Worksheet, cell: Cell) -> Series {
     let _ = sheet.name.withCString {
-      chart_series_set_name_range(lxw_chart_series, $0, cell.row, cell.col)
+      chart_series_set_name_range(lxwChartSeries, $0, cell.row, cell.col)
     }
     return self
   }
   /// Turn on a trendline for a chart data series.
   @discardableResult public func trendline(type: TrendlineType, value: Int = 2) -> Series {
-    chart_series_set_trendline(lxw_chart_series, type.rawValue, UInt8(value))
+    chart_series_set_trendline(lxwChartSeries, type.rawValue, UInt8(value))
     return self
   }
   /// Set the trendline name for a chart data series.
   @discardableResult public func trendline(name: String) -> Series {
-    let _ = name.withCString { chart_series_set_trendline_name(lxw_chart_series, $0) }
+    let _ = name.withCString { chart_series_set_trendline_name(lxwChartSeries, $0) }
     return self
   }
   /// Set the trendline line properties for a chart data series.
@@ -75,17 +75,17 @@ public struct Series {
     var line = lxw_chart_line(
       color: Color.black.hex, none: (hide ? 1 : 0), width: width, dash_type: UInt8(dash_type),
       transparency: UInt8(transparency))
-    chart_series_set_trendline_line(lxw_chart_series, &line)
+    chart_series_set_trendline_line(lxwChartSeries, &line)
     return self
   }
   /// Display the equation of a trendline for a chart data series.
   @discardableResult public func trendline_equation() -> Series {
-    chart_series_set_trendline_equation(lxw_chart_series)
+    chart_series_set_trendline_equation(lxwChartSeries)
     return self
   }
   /// Display the R squared value of a trendline for a chart data series.
   @discardableResult public func trendline_r_squared() -> Series {
-    chart_series_set_trendline_r_squared(lxw_chart_series)
+    chart_series_set_trendline_r_squared(lxwChartSeries)
     return self
   }
 }

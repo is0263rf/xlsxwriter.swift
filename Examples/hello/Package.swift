b168d9abdbe7b3d952a06891daa81cf653805dfc
Yoshinori Takada <is0263rf@gmail.com>
2024-08-30 14:04:02 +0900

    fix Examples/hello.

--- Examples/hello/Package.swift
+++ Examples/hello/Package.swift
@@ -4,12 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "hello",
-    dependencies: [.package(name: "xlsxwriter", path: "../..")],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "hello", dependencies: ["xlsxwriter"]),
-    ]
+  name: "hello",
+  dependencies: [
+    .package(name: "xlsxwriter", path: "../..")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "hello",
+      dependencies: ["xlsxwriter"]
+    )
+  ]
 )
--- Examples/hello/Sources/main.swift
+++ Examples/hello/Sources/main.swift
@@ -3,7 +3,7 @@ import xlsxwriter
 let workbook = Workbook(name: "hello_world.xlsx")
 let worksheet = workbook.addWorksheet()
 
-worksheet.write(.string("Hello"), .init(arrayLiteral: 0, 0), format: nil)
-worksheet.write(.number(123), .init(arrayLiteral: 1, 0), format: nil)
+worksheet.write(string: "Hello", .init(row: 0, col: 0), format: nil)
+worksheet.write(number: 123, .init(row: 1, col: 0), format: nil)
 
 workbook.close()

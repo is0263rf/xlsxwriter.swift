9e289d499be2b0baf1c5d8a6f02ebbdaa3543021
Yoshinori Takada <is0263rf@gmail.com>
2024-08-30 14:16:24 +0900

    fix Examples/anatomy.swift.

--- Sources/xlsxwriter/Cols.swift
+++ Sources/xlsxwriter/Cols.swift
@@ -7,17 +7,15 @@
 
 import libxlsxwriter
 
-public struct Cols: ExpressibleByStringLiteral, ExpressibleByArrayLiteral {
-  let col: UInt16, col2: UInt16
-  public init(stringLiteral value: String) {
-    (self.col, self.col2) = value.withCString { (lxw_name_to_col($0), lxw_name_to_col_2($0)) }
+public struct Cols {
+  let col: UInt16
+  let col2: UInt16
+
+  public init(cols: String) {
+    (self.col, self.col2) = cols.withCString { (lxw_name_to_col($0), lxw_name_to_col_2($0)) }
   }
-  public init(arrayLiteral elements: Int...) {
-    precondition(elements.count == 2, "[col, col2]")
-    self.col = UInt16(elements[0])
-    self.col2 = UInt16(elements[1])
-  }
-  init(_ col: UInt16, _ col2: UInt16) {
+
+  public init(_ col: UInt16, _ col2: UInt16) {
     self.col = col
     self.col2 = col2
   }

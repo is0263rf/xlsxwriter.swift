43d0aeda92ead449b1a3d28c57c17e439673a2f8
Yoshinori Takada <is0263rf@gmail.com>
2024-08-30 14:24:03 +0900

    fix Examples/format-font.

--- Examples/format-font/Package.swift
+++ Examples/format-font/Package.swift
@@ -4,12 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "format-font",
-    dependencies: [.package(name: "xlsxwriter", path: "../..")],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "format-font", dependencies: ["xlsxwriter"]),
-    ]
+  name: "format-font",
+  dependencies: [
+    .package(name: "xlsxwriter", path: "../..")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "format-font",
+      dependencies: ["xlsxwriter"]
+    )
+  ]
 )
--- Examples/format-font/Sources/main.swift
+++ Examples/format-font/Sources/main.swift
@@ -3,7 +3,7 @@ import xlsxwriter
 let workbook = Workbook(name: "format_font.xlsx")
 let worksheet = workbook.addWorksheet()
 
-worksheet.column(.init(arrayLiteral: 0, 0), width: 20, format: nil)
+worksheet.column(.init(0, 0), width: 20, format: nil)
 
 let format1 = workbook.addFormat()
 let format2 = workbook.addFormat()
@@ -14,8 +14,8 @@ format2.italic()
 format3.bold()
 format3.italic()
 
-worksheet.write(.string("This is bold"), .init(arrayLiteral: 0, 0), format: format1)
-worksheet.write(.string("This is italic"), .init(arrayLiteral: 1, 0), format: format2)
-worksheet.write(.string("Bold and italic"), .init(arrayLiteral: 2, 0), format: format3)
+worksheet.write(string: "This is bold", .init(row: 0, col: 0), format: format1)
+worksheet.write(string: "This is italic", .init(row: 1, col: 0), format: format2)
+worksheet.write(string: "Bold and italic", .init(row: 2, col: 0), format: format3)
 
 workbook.close()

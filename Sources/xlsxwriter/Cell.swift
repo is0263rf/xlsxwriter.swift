ed97b0d22a72bfb7ea8d9067af835461cbf1adc6
Yoshinori Takada <is0263rf@gmail.com>
2024-08-30 13:09:27 +0900

    fix Cell.swift.

--- Sources/xlsxwriter/Cell.swift
+++ Sources/xlsxwriter/Cell.swift
@@ -7,19 +7,16 @@
 
 import libxlsxwriter
 
-public struct Cell: ExpressibleByStringLiteral, ExpressibleByArrayLiteral {
-  let row: UInt32, col: UInt16
-  public init(stringLiteral value: String) {
-    (self.row, self.col) = value.withCString { (lxw_name_to_row($0), lxw_name_to_col($0)) }
-  }
-  public init(arrayLiteral elements: Int...) {
-    precondition(elements.count == 2, "[row, col]")
-    self.row = UInt32(elements[0])
-    self.col = UInt16(elements[1])
-  }
+public struct Cell {
+  let row: UInt32
+  let col: UInt16
 
-  public init(_ row: UInt32, _ col: UInt16) {
+  public init(row: UInt32, col: UInt16) {
     self.row = row
     self.col = col
+  }
+
+  public init(_ value: String) {
+    (self.row, self.col) = value.withCString { (lxw_name_to_row($0), lxw_name_to_col($0)) }
   }
 }

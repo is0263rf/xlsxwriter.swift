5b38ac45256c86890f41bfcfafb0381841940a94
Yoshinori Takada <is0263rf@gmail.com>
2024-09-02 10:48:00 +0900

    fix FormatScripts.swift.

--- Sources/xlsxwriter/FormatScripts.swift
+++ Sources/xlsxwriter/FormatScripts.swift
@@ -5,7 +5,11 @@
 //  Created by Yoshinori on 2024/08/30.
 //
 
+/// Superscript and subscript values
 public enum FormatScripts {
+  /// Superscript font
   case superscript
+
+  /// Subscript font
   case `subscript`
 }

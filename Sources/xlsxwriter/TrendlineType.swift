1292391a52aebf9d0ef19437bed6c10e23bf0fbb
Yoshinori Takada <is0263rf@gmail.com>
2024-09-02 10:00:02 +0900

    fix TrendlineType.swift.

--- Sources/xlsxwriter/TrendlineType.swift
+++ Sources/xlsxwriter/TrendlineType.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
+/// Series trendline/regression types.
 public enum TrendlineType: UInt8 {
+  /// Trendline type: Linear.
   case linear
+
+  /// Trendline type: Logarithm.
   case log
+
+  /// Trendline type: Polynomial.
   case poly
+
+  /// Trendline type: Power.
   case power
+
+  /// Trendline type: Exponential.
   case exp
+
+  /// Trendline type: Moving Average.
   case average
 }

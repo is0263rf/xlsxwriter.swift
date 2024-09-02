1f579d4c50929f3f1a03f18e71d6241ab433477b
Yoshinori Takada <is0263rf@gmail.com>
2024-09-02 09:46:30 +0900

    fix Range.swift.

--- Sources/xlsxwriter/Range.swift
+++ Sources/xlsxwriter/Range.swift
@@ -1,5 +1,5 @@
 //
-//  Cell_Range.swift
+//  Range.swift
 //
 //
 //  Created by Daniel Müllenborn on 02.01.21.
@@ -8,13 +8,18 @@
 import libxlsxwriter
 
 public struct Range: ExpressibleByStringLiteral, ExpressibleByArrayLiteral {
-  let row: UInt32, col: UInt16
-  let row2: UInt32, col2: UInt16
+  let row: UInt32
+  let col: UInt16
+  let row2: UInt32
+  let col2: UInt16
+
+  /// Convert an Excel A1:B2 range into a (first_row, first_col, last_row, last_col) sequence.
   public init(stringLiteral value: String) {
     (self.row, self.col, self.row2, self.col2) = value.withCString {
       (lxw_name_to_row($0), lxw_name_to_col($0), lxw_name_to_row_2($0), lxw_name_to_col_2($0))
     }
   }
+
   public init(arrayLiteral elements: Int...) {
     precondition(elements.count == 4, "[row, col, row2, col2]")
     self.row = UInt32(elements[0])

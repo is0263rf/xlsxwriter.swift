574dde66b560ce227a25166ea49cd713f5bbd937
Yoshinori Takada <is0263rf@gmail.com>
2024-09-02 11:02:35 +0900

    fix LegendPosition.swift.

--- Sources/xlsxwriter/LegendPosition.swift
+++ Sources/xlsxwriter/LegendPosition.swift
@@ -8,13 +8,30 @@
 import Foundation
 
 public enum LegendPosition: UInt8 {
+  /// No chart legend.
   case none = 0
+
+  /// Chart legend positioned at right side.
   case right
+
+  /// Chart legend positioned at left side.
   case left
+
+  /// Chart legend positioned at top.
   case top
+
+  /// Chart legend positioned at bottom.
   case bottom
-  case top_right
+
+  /// Chart legend positioned at top right.
+  case topRight
+
+  /// Chart legend positioned at right side.
   case overlayRight
+
+  /// Chart legend positioned at left side.
   case overlayLeft
+
+  /// Chart legend positioned at top right.
   case overlayTopRight
 }

a2188dbc291bbffacb99cad2af8c996d1cd6133b
Yoshinori Takada <is0263rf@gmail.com>
2024-09-02 10:28:01 +0900

    fix Align.swift.

--- Sources/xlsxwriter/Align.swift
+++ Sources/xlsxwriter/Align.swift
@@ -7,17 +7,41 @@
 
 import Foundation
 
+/// Alignment values
 public enum Align: UInt8 {
+  /// Left horizontal alignment
   case left
+
+  /// Center horizontal alignment
   case center
+
+  /// Right horizontal alignment
   case right
+
+  /// Center fill horizontal alignment
   case fill
+
+  /// Justify horizontal alignment
   case justify
+
+  /// Center Across horizontal alignment
   case centerAcross
+
+  /// Left horizontal alignment
   case distributed
+
+  /// Top vertical alignment
   case verticalTop
+
+  /// Bottom vertical alignment
   case verticalBottom
+
+  /// Center vertical alignment
   case verticalCenter
+
+  /// Justify vertical alignment
   case verticalJustify
+
+  /// Distributed vertical alignment
   case verticalDistributed
 }
